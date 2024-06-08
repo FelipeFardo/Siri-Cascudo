d87d5228a318693efdd10a802368f77c0a8faf11
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-06-07 22:16:42 -0300

    chore: app web config

--- apps/web/src/app/app/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default async function AppLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <div className="flex min-h-screen flex-col">{children}</div>
-}
--- apps/web/src/lib/trpc/react.ts
+++ apps/web/src/lib/trpc/react.ts
@@ -4,4 +4,3 @@ import { createTRPCReact } from '@trpc/react-query'
 export const trpc = createTRPCReact<AppRouter>()
 
 export const TRPCProvider = trpc.Provider
-
